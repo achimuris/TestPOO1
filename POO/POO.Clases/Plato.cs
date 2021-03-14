cef5998648a4932e9d8a1a987558c5e976a2da95
achimuris <chimuris@gmail.com>
2021-03-14 13:06:23 -0300

    Ejercicio01
    Agrego la clase Plato

---
+++ POO/POO.Clases/Plato.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO.Clases
+{
+    public class Plato
+    {
+        private int _cantidadDeIngredientes;
+
+        public int CantidadDeIngredientes
+        {
+            get { return _cantidadDeIngredientes; }
+            set { _cantidadDeIngredientes = value; }
+        }
+
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+
+        private double _precioBase;
+
+        public double PrecioBase
+        {
+            get { return _precioBase; }
+            set { _precioBase = value; }
+        }
+
+        public double getPrecio() 
+        {
+            double valorIngrediente = 0d;
+            if (this.CantidadDeIngredientes >3)
+            {
+                valorIngrediente = 30 * this.CantidadDeIngredientes;
+            }
+            else
+            {
+                valorIngrediente = 20 * this.CantidadDeIngredientes;
+            }
+
+            return valorIngrediente + this.PrecioBase;
+        }
+
+        public Plato()
+        {
+            this.PrecioBase = 150;
+        }
+
+        public Plato(string elNombre, int cantidadIngredientes)
+        {
+            this.Nombre = elNombre;
+            this.CantidadDeIngredientes = cantidadIngredientes;
+        }
+
+    }
+}
