b4e6a03b9c6cf29be9f1604956d1fa4de6ec3c94
achimuris <chimuris@gmail.com>
2021-03-14 13:38:48 -0300

    Ejercicio 2

---
+++ POO/POO.Clases/Ejercicio02/Plato.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+
+namespace POO.Clases.Ejercicio02
+{
+    public class Plato
+    {
+        private int _cantidadDeIngredientes;
+
+        public int CantidadDeIngredientes
+        {
+            get { return _cantidadDeIngredientes; }
+            set { _cantidadDeIngredientes = value; }
+        }
+
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+
+        private double _precioBase = 150;
+
+        public double PrecioBase
+        {
+            get { return _precioBase; }
+            set { _precioBase = value; }
+        }
+
+        public double getPrecio()
+        {
+            double valorIngrediente = 0d;
+            if (this.CantidadDeIngredientes > 3)
+            {
+                valorIngrediente = 30 * this.CantidadDeIngredientes;
+            }
+            else
+            {
+                valorIngrediente = 20 * this.CantidadDeIngredientes;
+            }
+
+            return valorIngrediente + this.PrecioBase;
+        }
+
+        public Plato()
+        {
+
+        }
+
+        public void AgregarOpinion(int unPuntaje)
+        {
+            _opiniones.Add(unPuntaje);
+        }
+
+        /// <summary>
+        /// Devuelve el promedio de las opiniones sobre ese plato
+        /// </summary>
+        /// <returns>promedio</returns>
+        public double getOpinionPromedio()
+        {
+            double promedio = -1;
+
+            if (this._opiniones.Count>0)
+            {
+                promedio = 0;
+                for (int i = 0; i < this._opiniones.Count; i++)
+                {
+                    promedio = promedio + this._opiniones[i];
+                }
+
+                promedio = promedio / this._opiniones.Count;
+            }
+
+
+            return promedio;
+        }
+
+        public Plato(string elNombre, int cantidadIngredientes)
+        {
+            this.Nombre = elNombre;
+            this.CantidadDeIngredientes = cantidadIngredientes;
+        }
+
+        private List<int> _opiniones = new List<int>();
+
+    }
+}
---
+++ POO/POO.Tests/UnitTestEjercicio2.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POO.Clases.Ejercicio02;
+
+namespace POO.Tests
+{
+    class UnitTestEjercicio2
+    {
+
+        [TestMethod]
+        public void TestMethodSinPromedio()
+        {
+            /*Resultados esperados*/
+            double resultadoEsperado = -1;
+
+            /*Ejecución*/
+            Plato objPlato = new Plato
+            {
+                Nombre = "Milanesas con papas fritas"
+            };
+
+            /*Assert - Prueba*/
+            Assert.AreEqual(resultadoEsperado, objPlato.getOpinionPromedio());
+        }
+
+
+        [TestMethod]
+        public void TestMethodPromedioSimple()
+        {
+            double resultadoEsperado = 7;
+
+
+            Plato objPlato = new Plato();
+            objPlato.Nombre = "Milanesas con papas fritas";
+            objPlato.AgregarOpinion(6);
+            objPlato.AgregarOpinion(7);
+            objPlato.AgregarOpinion(8);
+
+
+            Assert.AreEqual(resultadoEsperado, objPlato.getOpinionPromedio());
+        }
+
+
+        [TestMethod]
+        public void TestMethodNombreConConstructorGenerico()
+        {
+            Plato objPlato = new Plato();
+            objPlato.Nombre = "Milanesas con papas fritas";
+
+            Assert.AreEqual("Milanesas con papas fritas", objPlato.Nombre);
+
+        }
+
+        [TestMethod]
+        public void TestMethodNombreConConstructorEspecializado()
+        {
+            Plato objPlato = new Plato("Pollo a la parrilla", 5);
+
+            Assert.AreEqual("Pollo a la parrilla", objPlato.Nombre);
+
+        }
+
+        /// <summary>
+        /// TestMethodPrecioEscenario1() cantidad de ingredientes = 5
+        /// precio base = 150
+        /// precio ingredientes (por tener más de 3), 5 * 30 = 150
+        /// Debe retornar 300
+        /// </summary>
+        [TestMethod]
+        public void TestMethodPrecioEscenario1()
+        {
+
+            double resultadoEsperado = 300;
+            Plato objPlato = new Plato("Pollo a la parrilla", 5);
+
+            Assert.AreEqual(resultadoEsperado, objPlato.getPrecio());
+
+        }
+    }
+}
